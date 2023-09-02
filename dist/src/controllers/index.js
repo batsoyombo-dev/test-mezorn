0ec922c08d7f25f6e1f9b706bd3d1ee9a3c6f5a8
batsoyombo-dev <batsoyombo.kh@gmail.com>
2023-09-02 15:21:15 +0800

    dist removal

--- dist/src/controllers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TaskController = void 0;
-var task_controller_1 = require("./task.controller");
-Object.defineProperty(exports, "TaskController", { enumerable: true, get: function () { return __importDefault(task_controller_1).default; } });
